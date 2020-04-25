8d0a3b0cef70f27604ef74cbbec58f7a7c96a4bc
Roni <roni.c.castro@gmail.com>
2020-04-25 13:43:18 -0300

    Create database connection for dev and test server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import app from './app';
+import databaseConnection from './database/index';
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
+
+databaseConnection()
+  .then(_connection => {
+    console.log('Connected to database');
+  })
+  .catch(error => console.log('Error connection to database', error));
